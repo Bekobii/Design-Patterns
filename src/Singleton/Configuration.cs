bc511a85b0fffe6f99fe53b08f745315d5b8a08a
Adrian Dräger <adriandraeger03@gmail.com>
2024-02-28 16:31:52 +0100

    Added singleton pattern

---
+++ src/Singleton/Configuration.cs
@@ -0,0 +1,22 @@
+﻿namespace Singleton;
+
+public class Configuration
+{
+    
+    private static Configuration? Instance;
+
+
+    private Configuration()
+    {
+        
+    }
+
+    
+    public static Configuration GetInstance()
+    {
+        return Instance ??= new Configuration();
+    }
+    
+    
+    
+}
---
+++ src/Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Singleton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var firstInstance = Configuration.GetInstance();
+        var secondInstance = Configuration.GetInstance();
+
+        Console.WriteLine($"Instances equal: {firstInstance.Equals(secondInstance)}");
+    }
+}
